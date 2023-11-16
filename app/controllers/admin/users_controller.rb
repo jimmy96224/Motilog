2bc35cc5cb21b375b69cd188a4347deac7d28148
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-11-16 15:41:06 +0000

    [wip]admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,13 @@
 class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @diaries = @user.diaries.all
+    @instrument = @user.instruments.all
+  end
+
 end
