fc1b205b4ed5899534636aa382bc5034a08ca0e9
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-11-27 08:57:33 +0000

    [fix]diaries controller

--- app/controllers/public/diaries_controller.rb
+++ app/controllers/public/diaries_controller.rb
@@ -80,7 +80,7 @@ class Public::DiariesController < ApplicationController
   end
 
   def ensure_correct_user
-    @user = User.find(params[:id])
+    @user = User.find(params[:user_id])
     unless @user == current_user
       redirect_to user_path(current_user)
     end
